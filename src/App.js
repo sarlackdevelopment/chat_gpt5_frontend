504346cce4fb6a7bf96040d4d6b174fe85d6f31f
akorobov <akorobov@itquick.ru>
2023-12-10 16:05:56 +0100

    initialization

--- src/App.js
+++ src/App.js
@@ -1,23 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      It works!
+        <button onClick={(e) => fetch('http://localhost:5094/User/users').then((res) => console.log(res))}>GO!</button>
     </div>
   );
 }
